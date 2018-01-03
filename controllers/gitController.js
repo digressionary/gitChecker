71703fe5f794dc22668c0d3c772510fb3013fd4d
Drin Bee <wandererr@gmail.com>
2018-01-03 16:32:30 +0000

    working with rewire

--- controllers/gitController.js
+++ controllers/gitController.js
@@ -1,5 +1,5 @@
 
-var gitService = require('../services/gitService')();
+var gitService = require('../services/gitService')(); // parents at the end means create an object
 module.exports = function () {
     var userGet = function (req, res) {
         var userId = req.params.userId;
